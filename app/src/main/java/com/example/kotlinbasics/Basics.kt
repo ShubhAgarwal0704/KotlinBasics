59cd9641dc95d76890f9de39b9f136bed60435fe
Shubh <shubhagarwal0704@gmail.com>
2023-10-16 23:51:33 +0530

    kotlin basics - variables

---
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -0,0 +1,24 @@
+package com.example.kotlinbasics
+
+fun main()
+{
+    // this is a comment
+    println("Learning Kotlin Basics")
+    print("Hello World")
+    // function call
+    variables()
+}
+fun variables()
+{
+    // Variables
+    var name = "John" // mutable "var"    // String
+    val age = 25 // immutable "val"      // Int
+    var isAlive = true // Boolean
+    var marks = 97.4 // Double/ Float
+    println("Name: $name")
+    println("Age: $age")
+    println("isAlive: $isAlive")
+    println("Marks: $marks")
+}
+
+fun 
